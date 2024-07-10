1264ae6f5487d8262325c9fb6ea7dd473f5e9c17
lmstrand <lmstrand@gmail.com>
2024-07-10 14:20:50 +0300

    Add script

--- Koha/Plugin/Fi/KohaSuomi/QslipKeyShortcut.pm
+++ Koha/Plugin/Fi/KohaSuomi/QslipKeyShortcut.pm
@@ -22,7 +22,7 @@ our $metadata = {
     minimum_version => '24.05',
     maximum_version => '',
     version         => $VERSION,
-    description     => "Alt+P tulostaa tämän päivän lainat -kuitin (QSLIP)",
+    description     => "Alt+P tulostaa pikakuitin (tämän päivän lainat/QSLIP) lainaussivulla",
 };
 
 ## This is the minimum code required for a plugin's 'new' method
--- Koha/Plugin/Fi/KohaSuomi/QslipKeyShortcut/script.js
+++ Koha/Plugin/Fi/KohaSuomi/QslipKeyShortcut/script.js
@@ -1,1 +1,12 @@
-
+/// ALKU ///
+// Alt+P tulostaa pikakuitin (tämän päivän lainat) lainaussivulla
+$(document).ready(function () {
+    if (window.location.pathname == '/cgi-bin/koha/circ/circulation.pl') {
+        window.addEventListener("keydown", function (event) {
+            if (event.altKey && event.key === "p") {
+                window.open("/cgi-bin/koha/members/printslip.pl?borrowernumber=" + borrowernumber + "&amp;print=issueqslip", "printwindow");
+            }
+        });
+    }
+});
+/// LOPPU ///
